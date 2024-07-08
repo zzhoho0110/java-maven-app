def73fb5af5908355c4b29fda1794fdd878c3efe
zzhoho0110 <40865950+zzhoho0110@users.noreply.github.com>
2024-07-09 03:42:09 +0800

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,24 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def test(){
+   echo "testing..."
+   sh 'mvn test'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh 'docker build -t zzhoho0110/java-maven-app:jma-1.1.2 .'
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh 'docker push zzhoho0110/java-maven-app:jma-1.1.2'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
