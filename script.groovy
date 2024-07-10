493b7ef10dedd7519c58bbb97d054cfb3af88823
zzhoho0110 <abhoho0110@gmail.com>
2024-07-10 23:56:37 +0800

    Update Jenkinsfile, script.groovy

--- script.groovy
+++ script.groovy
@@ -1,12 +1,12 @@
 def buildJar() {
     echo "building the application..."
     sh 'mvn package'
-} 
+}
 
 def test(){
-   echo "testing..."
-   sh 'mvn test'
-} 
+    echo "testing..."
+    sh 'mvn test'
+}
 
 def buildImage() {
     echo "building the docker image..."
@@ -15,10 +15,10 @@ def buildImage() {
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh 'docker push zzhoho0110/java-maven-app:jma-1.1.2'
     }
-} 
+}
 
 def deployApp() {
     echo 'deploying the application...'
-} 
+}
 
 return this
